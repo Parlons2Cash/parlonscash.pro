2b302808f55de4dc4a93a5f6343a91f5bd6abf37
Parlons2Cash <azizguellouz@gmail.com>
2025-10-06 09:26:16 -0400

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,10 @@
 module.exports = function (eleventyConfig) {
+  // Dossiers ou fichiers à copier tels quels
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("Aziz Guellouz.jpg");
   eleventyConfig.addPassthroughCopy("ads.txt");
+  eleventyConfig.addPassthroughCopy("index.html");
+  eleventyConfig.addPassthroughCopy("blog.html");
 
   return {
     dir: {
